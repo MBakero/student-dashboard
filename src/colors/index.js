4ac0ae8ba31bc15c68a8175938b04ab65f25b3ac
MAIMOUN Bakr <bakero.maimoun@gmail.com>
2021-08-25 04:52:19 +0100

    color palette

---
+++ src/colors/index.js
@@ -0,0 +1,22 @@
+const colors = {
+	PURPLE: '#5855D6',
+	PURPLE_1: '#333269',
+	PURPLE_2: '#9592a6',
+	PURPLE_3: '#c0beff',
+	RED: '#FD3B2F',
+	PINK: '#FF2C55',
+	ORANGE: '#FF9501',
+	YELLOW: '#FFCC00',
+	GREEN: '#4ED964',
+	CYAN: '#5AC8FB',
+	BLUE: '#007AFF',
+	BLACK: '#000000',
+	GREY_1: '#8F8E94',
+	GREY_2: '#C8C7CD',
+	GREY_3: '#D2D1D7',
+	GREY_4: '#E6E5EB',
+	GREY_5: '#F0EFF5',
+	GREY_6: '#FEFEFE',
+}
+
+export default colors
